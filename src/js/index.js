79ae9ca9fe89c7aec7aafa9f6b9b477c0b65b731
山崎淳平 <62529950+pesso-yampee@users.noreply.github.com>
2021-03-21 10:58:23 +0900

    3桁区切りになるよう修正

--- src/js/index.js
+++ src/js/index.js
@@ -5,29 +5,29 @@
 // 見た目
 // まずは以下のレイアウトを作る
 
-const container           = document.getElementById('jsi-container');
-const result              = document.createElement('p');
-const mainBody            = document.createElement('div');
-const inner               = document.createElement('div');
-const other               = document.createElement('ul');
-const numbers             = document.createElement('ul');
-const operations          = document.createElement('ul');
-const otherItemTexts      = ['C', '+/-', '%'];
-const otherItemIds        = ['jsi-clear', 'jsi-changeSign', 'jsi-changePercentage'];
-const numbersItemTexts    = ['.', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const container = document.getElementById('jsi-container');
+const result = document.createElement('p');
+const mainBody = document.createElement('div');
+const inner = document.createElement('div');
+const other = document.createElement('ul');
+const numbers = document.createElement('ul');
+const operations = document.createElement('ul');
+const otherItemTexts = ['C', '+/-', '%'];
+const otherItemIds = ['jsi-clear', 'jsi-changeSign', 'jsi-changePercentage'];
+const numbersItemTexts = ['.', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const operationsItemTexts = ['÷', '×', '-', '+', '='];
-const operationsItemIds   = ['jsi-divide', 'jsi-multiply', 'jsi-minus', 'jsi-plus', 'jsi-equal'];
+const operationsItemIds = ['jsi-divide', 'jsi-multiply', 'jsi-minus', 'jsi-plus', 'jsi-equal'];
 
-let otherItemArray   = [];
+let otherItemArray = [];
 let numbersItemArray = [];
-let operationsItemArray  = [];
+let operationsItemArray = [];
 
-container.classList  = 'container';
-result.classList     = 'result';
-mainBody.classList   = 'main-body';
-inner.classList      = 'inner';
-other.classList      = 'other';
-numbers.classList    = 'numbers';
+container.classList = 'container';
+result.classList = 'result';
+mainBody.classList = 'main-body';
+inner.classList = 'inner';
+other.classList = 'other';
+numbers.classList = 'numbers';
 operations.classList = 'operations';
 
 result.textContent = 0;
@@ -43,7 +43,7 @@ for (let i = 0; i < otherItemTexts.length; i++) {
 
 for (let i = 0; i < 11; i++) {
   const numbersItem = document.createElement('li');
-  
+
   if (i === 1) {
     numbersItem.classList = 'button button--colon';
     numbersItem.id = 'jsi-colon';
@@ -57,7 +57,7 @@ for (let i = 0; i < 11; i++) {
   } else if (i === 1) {
     numbersItem.textContent = '.';
   } else {
-    numbersItem.textContent = i-1;
+    numbersItem.textContent = i - 1;
   }
 
   numbersItemArray.push(numbersItem);
@@ -102,16 +102,21 @@ for (let i = 0; i < numbersChildren.length; i++) {
   // 変数numbersChildrenのi番目の要素を取得
   const number = numbersChildren.item(i);
 
-  number.addEventListener('click', function() {
+  number.addEventListener('click', function () {
     const clickedNumber = this.textContent;
 
     if (result.textContent === '0') {
-      result.textContent = Number(clickedNumber).toLocaleString();
+      // 正規表現を使ってカンマ区切りの数値を実現
+      result.textContent = clickedNumber;
     } else {
-      // toLocaleString()関数を用いて数値表現を使う
-      // ただ5桁以上になるとNaNになる
-      result.textContent = Number(result.textContent + clickedNumber).toLocaleString();
-      console.log(result);
+      result.textContent = (result.textContent + clickedNumber).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+      // このままだとカンマ区切りされた文字列に文字列を足していくことになるから、結果的にカンマが何個も増えることになる。
+      // もし文字列の長さが6以上(カンマ含めむ)なら入力したタイミングでカンマを外して、通常の文字列に直す。
+      if (result.textContent.length >= 6) {
+        const removedComma = result.textContent.replace(/,/g, '');
+        result.textContent = removedComma.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        console.log(removedComma);
+      }
     }
   });
 }
